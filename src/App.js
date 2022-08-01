7cbda3a8f08bd050fda10d03a493bf459380ccee
yetabalij <ermiastsegu@yahoo.com>
2022-08-01 12:50:37 +0300

    tailwind installed and configured.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App">Hello, Ermi</div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
