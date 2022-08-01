47e99f06f39cef123e710b41790390df4d5b9926
yetabalij <ermiastsegu@yahoo.com>
2022-08-01 15:01:31 +0300

    color configuration added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#0E1126",
+        "secondary-color": "#6387F2",
+        "third-color": "#141259",
+        "fourth-color": "#F2D230",
+      },
+    },
   },
   plugins: [],
 };
