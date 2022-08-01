3f7188a63212e2f494b99c614e2ca2cd30818236
yetabalij <ermiastsegu@yahoo.com>
2022-08-01 18:05:48 +0300

    modified.

--- src/Section/Home/Home.js
+++ src/Section/Home/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Ermias from "./../../assets/Ermias.png";
 
 const Home = () => {
-  return <section id="home">Home</section>;
+  return (
+    <section className="flex justify-between pt-10 " id="home">
+      <div className="opacity-10 text-[12rem] font-bold tracking-widest">
+        HELLO
+      </div>
+
+      <div>
+        <img alt="Ermias" src={Ermias} className="w-full" />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
