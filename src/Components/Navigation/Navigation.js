284a7061aad3334380d7f2ea1763d52aa0f1727f
yetabalij <ermiastsegu@yahoo.com>
2022-08-01 14:12:01 +0300

    modified.

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Link } from "react-scroll";
+
+const Navigation = () => {
+  return (
+    <div className="absolute top-[85%] left-[50%] translate-x-[-50%] bg-red-200 px-6 py-4 rounded-full">
+      <ul className="flex gap-2">
+        <li>
+          <Link to="home" spy={true} smooth={true}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" spy={true} smooth={true}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skill" spy={true} smooth={true}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="project" spy={true} smooth={true}>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" spy={true} smooth={true}>
+            Contacts
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
