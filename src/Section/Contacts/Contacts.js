6d6f4345305958289492f8ae423b0c941b1e9d97
yetabalij <ermiastsegu@yahoo.com>
2022-08-01 12:57:47 +0300

    section files are created.

---
+++ src/Section/Contacts/Contacts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contacts = () => {
+  return <section>Contacts</section>;
+};
+
+export default Contacts;
---
+++ src/Section/Skills/Skills.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Skills = () => {
+  return <section>Skills</section>;
+};
+
+export default Skills;
