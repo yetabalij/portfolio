fd43ee653edfc8dd6e8f788499289ac265097ba6
Ermias <yetabalij@gmail.com>
2022-08-02 14:23:56 +0300

    style changed.

--- src/Section/Home/Home.js
+++ src/Section/Home/Home.js
@@ -3,8 +3,11 @@ import Ermias from "./../../assets/Ermias.png";
 
 const Home = () => {
   return (
-    <section className="flex justify-between pt-10 " id="home">
-      <div className="opacity-10 text-[12rem] font-bold tracking-widest">
+    <section
+      className="sm:flex justify-between pt-10 overflow-hidden "
+      id="home"
+    >
+      <div className="opacity-10 text-[4rem] sm:text-[5.5rem] md:text-[10rem] font-bold tracking-widest">
         HELLO
       </div>
 
