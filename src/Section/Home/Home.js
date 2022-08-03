ccf62da9208a164e1329d859ac72b542bb0b8397
yetabalij <ermiastsegu@yahoo.com>
2022-08-04 00:16:50 +0300

    content added.

--- src/Section/Home/Home.js
+++ src/Section/Home/Home.js
@@ -3,15 +3,29 @@ import Ermias from "./../../assets/Ermias.png";
 
 const Home = () => {
   return (
-    <section className="sm:flex gap-1 mt-10 overflow-hidden h-screen" id="home">
+    <section
+      className="sm:flex justify-between align-center mt-10 overflow-hidden h-screen"
+      id="home"
+    >
       <div className="">
-        <h1 className="opacity-10 text-[4rem] sm:text-[5.5rem] md:text-[10rem] font-bold tracking-widest">
+        <p className="opacity-10 text-[4rem] sm:text-[5.5rem] md:text-[6rem] lg:text-[8rem] xl:text-[10rem] font-bold tracking-widest">
           HELLO
-        </h1>
-        <p>I'am</p>
+        </p>
+        <div className="">
+          <p className="text-4xl">I'm</p>
+          <div className="text-4xl mt-4 flex gap-2">
+            <div>
+              <p>Ermias</p>
+              <span className="border-b-2 border-fourth-color border-solid w-20 content-none block mt-2 ml-1"></span>
+            </div>
+            <p className="block">
+              <span className="font-bold text-secondary-color">Tsegu</span>
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div>
+      <div className="sm:w-1/3 md:w-1/2">
         <img alt="Ermias" src={Ermias} />
       </div>
     </section>
