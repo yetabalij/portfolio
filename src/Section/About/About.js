17210e0a0c37b79e83c7182ec7a5ecefdd4ac0f0
yetabalij <yetabalij@gmail.com>
2022-08-30 09:27:14 +0300

    new component integrated.

--- src/Section/About/About.js
+++ src/Section/About/About.js
@@ -1,7 +1,14 @@
 import React from "react";
+import LeftSection from "./components/LeftSection";
+import RightSection from "./components/RightSection";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <LeftSection />
+      <RightSection />
+    </section>
+  );
 };
 
 export default About;
