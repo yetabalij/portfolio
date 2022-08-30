271e9fa15f86cb0442d9c0cdef02608c8456a56e
yetabalij <yetabalij@gmail.com>
2022-08-30 12:22:06 +0300

    navigation component is changed.

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -9,34 +9,32 @@ import { Link } from "react-scroll";
 
 const Navigation = () => {
   return (
-    <div className="fixed top-[85%] left-[50%] translate-x-[-50%] bg-fourth-color text-primary-color px-6 py-3 rounded-full z-10">
-      <ul className="flex gap-4">
-        <li className="cursor-pointer">
-          <Link to="home" spy={true} smooth={true}>
-            <AiOutlineHome className="text-xl" />
-          </Link>
-        </li>
-        <li className="cursor-pointer">
-          <Link to="about" spy={true} smooth={true}>
-            <MdPersonOutline className="text-xl" />
-          </Link>
-        </li>
-        <li className="cursor-pointer">
-          <Link to="skill" spy={true} smooth={true}>
-            <VscTools className="text-xl" />
-          </Link>
-        </li>
-        <li className="cursor-pointer">
-          <Link to="project" spy={true} smooth={true}>
-            <GoFileCode className="text-xl" />
-          </Link>
-        </li>
-        <li className="cursor-pointer">
-          <Link to="contact" spy={true} smooth={true}>
-            <AiOutlineMail className="text-xl" />
-          </Link>
-        </li>
-      </ul>
+    <div className="bg-fourth-color text-primary-color flex justify-center gap-4 rounded-full py-3 px-10">
+      <li className="cursor-pointer">
+        <Link to="home" spy={true} smooth={true}>
+          <AiOutlineHome className="text-3xl" />
+        </Link>
+      </li>
+      <li className="cursor-pointer">
+        <Link to="about" spy={true} smooth={true}>
+          <MdPersonOutline className="text-3xl" />
+        </Link>
+      </li>
+      <li className="cursor-pointer">
+        <Link to="skill" spy={true} smooth={true}>
+          <VscTools className="text-3xl" />
+        </Link>
+      </li>
+      <li className="cursor-pointer">
+        <Link to="project" spy={true} smooth={true}>
+          <GoFileCode className="text-3xl" />
+        </Link>
+      </li>
+      <li className="cursor-pointer">
+        <Link to="contact" spy={true} smooth={true}>
+          <AiOutlineMail className="text-3xl" />
+        </Link>
+      </li>
     </div>
   );
 };
