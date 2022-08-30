219339e14e7d4fe6c6ab4c489e9cb2b6e1e9e67d
yetabalij <yetabalij@gmail.com>
2022-08-30 15:58:45 +0300

    changed.

--- src/Section/About/About.js
+++ src/Section/About/About.js
@@ -4,13 +4,11 @@ import RightSection from "./components/RightSection";
 
 const About = () => {
   return (
-    <section id="about" className="mt-24">
+    <section id="about" className="">
       <div className="flex justify-center ">
-        <div className="">
-          <h1 className="opacity-[.4] text-[4rem] sm:text-[5.5rem] md:text-[6rem] lg:text-[7rem] xl:text-[10rem] 2xl:text-[12rem] font-bold tracking-widest">
-            ABOUT
-          </h1>
-        </div>
+        <h1 className="opacity-[.4] text-[4rem] sm:text-[5.5rem] md:text-[6rem] lg:text-[7rem] xl:text-[10rem] 2xl:text-[12rem] font-bold tracking-widest">
+          ABOUT
+        </h1>
       </div>
       <div className="flex justify-between">
         <LeftSection />
