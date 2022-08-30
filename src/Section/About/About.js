c005c8c800e7b8fcdef8f413a9e4aaa68a568a8f
yetabalij <yetabalij@gmail.com>
2022-08-30 12:22:20 +0300

    about component is modified.

--- src/Section/About/About.js
+++ src/Section/About/About.js
@@ -4,9 +4,18 @@ import RightSection from "./components/RightSection";
 
 const About = () => {
   return (
-    <section id="about">
-      <LeftSection />
-      <RightSection />
+    <section id="about" className="mt-24">
+      <div className="flex justify-center ">
+        <div className="">
+          <h1 className="opacity-[.4] text-[4rem] sm:text-[5.5rem] md:text-[6rem] lg:text-[7rem] xl:text-[10rem] 2xl:text-[12rem] font-bold tracking-widest">
+            ABOUT
+          </h1>
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <LeftSection />
+        <RightSection />
+      </div>
     </section>
   );
 };
