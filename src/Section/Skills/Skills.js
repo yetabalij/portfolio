60a2e096028566bf01340383a5be28e1b1d07dba
yetabalij <yetabalij@gmail.com>
2022-09-01 16:18:34 +0300

    title element changed.

--- src/Section/Skills/Skills.js
+++ src/Section/Skills/Skills.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Title from "./../../Components/Title";
 
 const Skills = () => {
-  return <section id="skill">Skills</section>;
+  return (
+    <section id="skill">
+      <Title title="SKILLS" />
+    </section>
+  );
 };
 
 export default Skills;
