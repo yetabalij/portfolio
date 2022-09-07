e3507f697bb97a295219ea263112d2933f0b4a74
yetabalij <yetabalij@gmail.com>
2022-09-07 10:25:53 +0300

    component started.

--- src/Section/Projects/Projects.js
+++ src/Section/Projects/Projects.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Title from "../../Components/Title";
+import TravelAddis from "./../../assets/traveladdis.png";
+import RealTime from "./../../assets/realtimebook.png";
 
 const Projects = () => {
-  return <section id="project">Projects</section>;
+  return (
+    <section id="project">
+      <div>
+        <Title title="PROJECTS" />
+        <div className="flex flex-col gap-y-4">
+          <div className="sm:flex justify-center">
+            <div className="sm:w-1/2 bg-gradient-to-r from-cyan-500 to-[#6387F2] justify-center items-center flex">
+              description
+            </div>
+            <div className="sm:w-1/2 ">
+              <img src={TravelAddis} alt=""></img>
+            </div>
+          </div>
+          <div className="sm:flex justify-center">
+            <div className="sm:w-1/2">
+              <img src={RealTime} alt="" />
+            </div>
+            <div className="sm:w-1/2 bg-gradient-to-r from-cyan-500 to-blue-500 justify-center items-center flex">
+              description
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
