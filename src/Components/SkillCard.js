678549d035adb2f6884400e9a392c3f171880516
yetabalij <yetabalij@gmail.com>
2022-09-07 09:45:22 +0300

    style modified and text description added.

--- src/Components/SkillCard.js
+++ src/Components/SkillCard.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const SkillCard = (props) => {
   return (
-    <div className="bg-third-color p-8 rounded-xl sm:w-[18%] mb-2 sm:mb-4">
+    <div className="bg-third-color p-8 rounded-xl sm:w-[18%] mb-2 sm:mb-4 flex flex-col justify-center items-center sm:text-sm lg:text-xl text-gray-300 font-medium">
       {props.children}
+      <h3 className="text-center mt-5">{props.name}</h3>
     </div>
   );
 };
