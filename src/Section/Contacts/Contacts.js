a98f04ab8f93ac1abdcf6f1e92582cff45aaa6b9
Ermias <yetabalij@gmail.com>
2022-09-09 16:22:43 +0300

    contact us title.

--- src/Section/Contacts/Contacts.js
+++ src/Section/Contacts/Contacts.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Title from "./../../Components/Title";
 
 const Contacts = () => {
-  return <section id="contact">Contacts</section>;
+  return (
+    <section id="contact">
+      <Title title="CONTACTS" />
+    </section>
+  );
 };
 
 export default Contacts;
