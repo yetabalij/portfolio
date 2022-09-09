37a95d72ee6309e1e9689f117c66b1926c09a3b9
Ermias <yetabalij@gmail.com>
2022-09-09 17:15:21 +0300

    contact us started.

--- src/Section/Contacts/Contacts.js
+++ src/Section/Contacts/Contacts.js
@@ -5,6 +5,11 @@ const Contacts = () => {
   return (
     <section id="contact">
       <Title title="CONTACTS" />
+      <div>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 p-1 rounded-full">
+          <input className="w-full text-black rounded-full" type="text" />
+        </div>
+      </div>
     </section>
   );
 };
