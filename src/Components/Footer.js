8bb7e1ff07c4fb5af0b75b9644ab770a6152d1a6
Ermias <yetabalij@gmail.com>
2022-09-11 15:41:45 +0300

    modified.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-full bg-third-color py-10">
+    <div className="h-full bg-third-color py-10 overflow-hidden">
       <div className="w-[80%] mx-auto"> Footer</div>
     </div>
   );
