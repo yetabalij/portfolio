f896918fcc956bde14203b30cde331e4c81e60db
Ermias <yetabalij@gmail.com>
2022-09-11 12:42:56 +0300

    font size modified.

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Title = (props) => {
   return (
-    <div className="flex justify-center mt-20">
-      <h1 className="opacity-[.3] text-[2rem] sm:text-[5.5rem] md:text-[6rem] lg:text-[7rem] xl:text-[10rem] 2xl:text-[11rem] font-bold tracking-widest">
+    <div className="flex justify-center my-10">
+      <h1 className="opacity-[.3] text-[2rem] sm:text-[5.5rem] md:text-[6rem] lg:text-[7rem] xl:text-[8rem] 2xl:text-[10rem] font-bold tracking-widest">
         {props.title}
       </h1>
     </div>
