406973cba2ffe619618bf87da6f5357888c19c16
yetabalij <yetabalij@gmail.com>
2022-09-28 15:20:07 +0300

    title fonts fixed.

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 const Title = (props) => {
   return (
     <div className="flex justify-center my-10">
-      <h1 className="opacity-[.3] text-[2rem] sm:text-[5.5rem] md:text-[6rem] lg:text-[7rem] xl:text-[8rem] 2xl:text-[10rem] font-bold tracking-widest">
+      <h1 className="opacity-[.3] text-[2rem] sm:text-[5.5rem] md:text-[5rem] lg:text-[6rem] xl:text-[7rem] 2xl:text-[7rem] font-bold tracking-widest">
         {props.title}
       </h1>
     </div>
